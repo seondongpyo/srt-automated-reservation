622504f7d3ade9fc79bbab33f44783c2da0524a0
Seondong Pyo <psd1390@real-iv.co.kr>
2023-01-16 18:00:13 +0900

    test: 일반실 예약대기 신청 테스트

--- src/test/java/io/github/seondongpyo/page/SrtIntegrationTest.java
+++ src/test/java/io/github/seondongpyo/page/SrtIntegrationTest.java
@@ -66,4 +66,44 @@ class SrtIntegrationTest {
             refresh();
         }
     }
+    
+    @DisplayName("일반실 예약대기")
+    @Test
+    void waitingForReservation() throws IOException {
+        List<String> accountInfo = Files.readAllLines(Paths.get("account.txt"));
+        String id = accountInfo.get(0);
+        String password = accountInfo.get(1);
+
+        SrtLoginPage loginPage = open(SrtLoginPage.URL, SrtLoginPage.class);
+        SrtMainPage mainPage = loginPage.login(id, password);
+        SrtTicketListPage ticketListPage = mainPage.search("수서", "정읍", 2, "20230121", "000000");
+
+        int count = 1;
+        while (true) {
+            ElementsCollection searchResults = ticketListPage.getSearchResults();
+            Optional<SelenideElement> maybeResult = searchResults.asDynamicIterable()
+                .stream()
+                .filter(result -> {
+                    List<String> times = List.of("08:33", "11:00", "14:10");
+                    String time = result.find("td:nth-child(4) > em").text();
+                    String buttonText = result.find("td", 7).text();
+                    return times.contains(time) && buttonText.contains("신청하기");
+                })
+                .findFirst();
+
+            if (maybeResult.isPresent()) {
+                SelenideElement result = maybeResult.get();
+                SelenideElement button = result.find("td:nth-child(8) > a");
+                executeJavaScript("arguments[0].click();", button);
+
+                webdriver().shouldHave(WebDriverConditions.title("예약하기 < 승차권예약 < 승차권 < 승차권 예약/발매 - 국민철도 SR"));
+                $("#list-form > fieldset > div.tal_c > a.btn_large.btn_blue_dark.val_m.mgr10").shouldHave(visible).click();
+                break;
+            }
+
+            String currentDateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSS"));
+            System.out.printf("[%s] (%d) 검색 결과 없으므로 새로고침%n", currentDateTime, count++);
+            refresh();
+        }
+    }
 }
